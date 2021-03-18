fcb64858f18c3130eb4356906ab3e86e9f663f87
Watcharapol Tadtiang <watcharapol.tadtiang@itone.co.th>
2021-03-18 18:56:52 +0700

    improvement login service

--- lib/src/service/login_service.dart
+++ lib/src/service/login_service.dart
@@ -5,57 +5,51 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
-final GoogleSignIn googleSignIn = GoogleSignIn(
-    scopes: [
-      'email'
-    ]
-);
+final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
 
 class LoginService {
   int _expireInSeconds = 3600;
 
   Future<void> login(BuildContext context) async {
     cache.write('isLoggedIn', 'true', _expireInSeconds);
-    await Navigator.of(context)
-        .pushNamedAndRemoveUntil('/home', (route) => false);
+    await Navigator.of(context).pushNamedAndRemoveUntil('/home', (route) => false);
   }
 
   Future user() async {
-    return cache.load('user', '{"user": "test", "firstname": "test", "lastname": "test"}');
+    return cache.load('user', '{"name": "test", "email": "test@test.com"}');
   }
 
   Future<void> logout(BuildContext context) async {
     cache.destroy('isLoggedIn');
-    cache.destroy('token');
     cache.destroy('user');
-    await googleSignIn.signOut();
-    await Navigator.of(context)
-        .pushNamedAndRemoveUntil('/login', (route) => false);
+    await _googleSignIn.signOut();
+    await Navigator.of(context).pushNamedAndRemoveUntil('/login', (route) => false);
   }
 
   Future isLogin() async {
-    return cache.load('isLoggedIn', 'false');
+    var result = await cache.load('isLoggedIn', 'false');
+    if (result == 'true') {
+      cache.write('isLoggedIn', 'true', _expireInSeconds);
+    }
+    return result;
   }
 
   Future<String> signInWithGoogle() async {
     await Firebase.initializeApp();
 
-    final GoogleSignInAccount googleSignInAccount = (await googleSignIn.signIn())!;
-    final GoogleSignInAuthentication googleSignInAuthentication =
-    await googleSignInAccount.authentication;
+    final GoogleSignInAccount googleSignInAccount = (await _googleSignIn.signIn())!;
+    final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
 
     final AuthCredential credential = GoogleAuthProvider.credential(
       accessToken: googleSignInAuthentication.accessToken,
       idToken: googleSignInAuthentication.idToken,
     );
 
-    final UserCredential authResult =
-    await _auth.signInWithCredential(credential);
+    final UserCredential authResult = await _auth.signInWithCredential(credential);
     final User user = authResult.user!;
     var userMap = {"name": user.displayName, "email": user.email};
     cache.write('isLoggedIn', 'true', _expireInSeconds);
     cache.write('user', userMap, _expireInSeconds);
     return '$user';
   }
-
 }
